607efc42acfb8b4717962859269a4b07edd9f925
Allison Christiansen <allison@nnyit.com>
2021-02-17 17:36:57 -0500

    Initial javascript calculateWinners Kata function

---
+++ CodeWarsKatas.js
@@ -0,0 +1,8 @@
+/**
+ * https://www.codewars.com/kata/6022c97dac16b0001c0e7ccc/train/javascript
+ * @param snapshot
+ * @param penguins
+ */
+function calculateWinners(snapshot, penguins) {
+    // TODO: solve kata
+};
